3c2d93a28ac30c37c7d42d68456f3c91568f12c9
MahamZafar <mahamzafar56@gmail.com>
2023-10-10 18:22:49 -0700

    added react forms

---
+++ Week6/react_forms_ecentennial/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Controlled from "./components/Controlled/Controlled";
+
+  const App = () => {
+    return (
+      <div>
+        {<Controlled />}
+      </div>
+    );
+  };
+export default App
